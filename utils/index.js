62687bc049d0fe01d52b870c8b19af5cb3d3a7b0
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-17 23:54:30 +0800

    removed the substring limit

--- utils/index.js
+++ utils/index.js
@@ -12,7 +12,7 @@ const createResponse = (intent, tvshow) => {
 
     switch(intent) {
       case 'tvInfo' : {
-        let str = `${Title} (${Year}). This film was directed by ${Director} and starred ${Actors}. ${Plot}`.substring(0,320);
+        let str = `${Title} (${Year}). This film was directed by ${Director} and starred ${Actors}. ${Plot}`;
         return {
           text: str,
           image: Poster

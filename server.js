0cf0f1379ddea51adcab9a3e6af01e7db0af4d4c
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-18 14:53:18 +0800

    sdsa

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ server.post('/', (req, res, next) => {
 
 			//WIT Message API
 			wit.message(message.text, {})
-				.then(response => console.log(response.entities))
 				.then(omdb)
 				.then(response => {
 					f.txt(sender, response.text);

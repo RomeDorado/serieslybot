92b91139ad9b389dccdc7940b1b0800ae785a916
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-19 15:57:37 +0800

    asd

--- omdb/index.js
+++ omdb/index.js
@@ -37,9 +37,9 @@ const getInfo = data => {
         },
         method: 'GET'
       }, (error, response, body) => {
-        console.log(JSON.parse(body[0]));
+        console.log(JSON.parse(body));
         if(!error && response.statusCode === 200){
-          resolve(createResponse(intent, JSON.parse(body[0])));
+          resolve(createResponse(intent, JSON.parse(body)));
         } else{
           reject(error);
         }

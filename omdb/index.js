65626ca0375beefb1480f59a23de69f9283cb951
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-19 15:11:02 +0800

    asdfgh

--- omdb/index.js
+++ omdb/index.js
@@ -29,6 +29,7 @@ const getInfo = data => {
       });
     }
     else if(person != null){
+      console.log("lols");
       //Fetch data from TMDB
       request({
         uri: "https://api.themoviedb.org/3/search/person?api_key=92b2df3080b91d92b31eacb015fc5497",

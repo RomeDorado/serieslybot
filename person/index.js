6e88a8b0d900607b400a29683e7cf62b858217e7
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-20 10:26:54 +0800

    asddfgh

--- person/index.js
+++ person/index.js
@@ -7,14 +7,16 @@ const createResponsePerson = (intent, person) => {
       results: [{
       name,
       profile_path,
-      known_for
+      known_for{
+        title
+      }
       }]
 
     } = person;
     switch(intent){
 
       case 'personInfo': {
-        let str = `${name} is an actor. and he is know for the movie ${known_for}`;
+        let str = `${name} is an actor. and he is know for the movie ${title}`;
         return{
           text: str,
           image: profile_path

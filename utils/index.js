1791e0e3bdd49118cb5a1e00b832c1947fe807df
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-20 14:18:13 +0800

    asdfg

--- utils/index.js
+++ utils/index.js
@@ -18,7 +18,7 @@ const createResponse = (intent, tvshow) => {
 
     switch(intent) {
       case 'tvInfo' : {
-        let str = `${Title} (${Year}). This film was directed by ${Director} and starred ${Actors}. ${Plot}`;
+        let str = `${Title} (${Year}). This film was directed by ${Director} and starred ${Actors}. ${Plot}`.subString(0, 360);
         return {
           text: str,
           image: Poster
@@ -108,4 +108,3 @@ const createResponsePerson = (intent, person) => {
 
 */
 module.exports = createResponse;
-

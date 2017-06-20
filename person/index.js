fc65c7a94fe40cd3b560584d056fd3584db43cda
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-20 13:38:04 +0800

    id

--- person/index.js
+++ person/index.js
@@ -15,9 +15,8 @@ const createResponsePerson = (intent, person) => {
       } = person;
 
       request({
-        uri: "https://api.themoviedb.org/3/person/",
+        uri: "https://api.themoviedb.org/3/person/"+id,
         qs: {
-          id,
           api_key: "92b2df3080b91d92b31eacb015fc5497",
         },
         method: "GET"

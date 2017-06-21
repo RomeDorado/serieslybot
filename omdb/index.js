5ca351e4af5d8611377309a19ef2719c28acc8c4
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-21 13:32:08 +0800

    Added fetch movie list query

--- omdb/index.js
+++ omdb/index.js
@@ -2,11 +2,59 @@
 const request = require('request');
 const createResponse = require('../utils');
 const createResponsePerson = require('../person');
+const createMovieList = require('../movieList');
 const config = require('../config');
 const getInfo = data => {
   let intent = data.entities.intent && data.entities.intent[0].value || 'tvInfo';
   let tvshow = data.entities.tvshow && data.entities.tvshow[0].value || null;
   let person = data.entities.person && data.entities.person[0].value || null;
+  let genre = data.entities.genre && data.entities.genre[0].value || null;
+  let year = data.entities.year && data.entities.year[0].value || null;
+  var genreID = "";
+
+  //Get the Genre ID
+  switch(genre){
+      case "Action":
+      genreID = 28;
+      break;
+
+      case "Adventure":
+      genreID = 12;
+      break;
+
+      case "Animation":
+      genreID = 16;
+      break;
+
+      case "Comedy":
+      genreID = 35;
+      break;
+
+      case "Drama":
+      genreID = 18;
+      break;
+
+      case "Horror":
+      genreID = 27;
+      break;
+
+      case "Fantasy":
+      genreID = 14;
+      break;
+
+      case "Music":
+      genreID = 10402;
+      break;
+
+      case "Romance":
+      genreID = 10749;
+      break;
+
+      case "Science Fiction":
+      genreID = 878;
+      break;
+  }
+
   return new Promise((resolve, reject) => {
     console.log(intent + " " + tvshow + " " + person);
     if(tvshow != null) {
@@ -42,6 +90,27 @@ const getInfo = data => {
         console.log(JSON.parse(body));
         if(!error && response.statusCode === 200){
           resolve(createResponsePerson(intent, JSON.parse(body)));
+        } else{
+          reject(error);
+        }
+      });
+    }
+    else if(year != null || genre != null){
+      request({
+        uri: "https://api.themoviedb.org/3/discover/tv",
+        qs: {
+          api_key: config.TMDB_API_KEY,
+          language: en-US,
+          sort_by: popularity.desc,
+          page: '1',
+          // with_genres: genreID,
+          primary_release_year: year
+        },
+        method: 'GET'
+      }, (error, response, body) => {
+        console.log(JSON.parse(body));
+        if(!error && response.statusCode === 200){
+          resolve(createMovieList(JSON.parse(body)));
         } else{
           reject(error);
         }

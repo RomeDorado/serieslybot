720d3534373af16054f43bd6d2c9ee3820175f24
RomeDorado <romedorado@gmail.com>
2017-06-22 00:12:01 +0800

    showing

--- showingList/index.js
+++ showingList/index.js
@@ -17,7 +17,7 @@ const createShowingList = (showingList) => {
       }]
     } = showingList;
     
-    console.log(JSON.parse(items) + "this is the name");
+    console.log(JSON.parse(listitem) + "this is the name");
     let str = `List of now showing movies: ${name}`.substring(0,320);
 
     return{

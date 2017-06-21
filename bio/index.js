1107bca3faf6225ab5824a581b1f2b90994e10b9
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-21 10:56:31 +0800

    bio added

--- bio/index.js
+++ bio/index.js
@@ -11,18 +11,28 @@ const createBiography = (bio) => {
       biography,
     } = bio;
     var s ='';
+    //console.log(biography.length + "this is the length of chars");
     var biog = [] = biography.split(".");
     //let str = `${name}, ${biography}`.substring(0,400);
-    let str = `${name},`;
-    for (var i=1; i <= 5; i++){
+   let str = `${name},`;
+    for (var i=1; i <= 3; i++){
 
-      s += biog[i];
+     s += biog[i] + ".";
 
-    }
+   }
 
+   if(s.length > 500){
+
+
+     s = biog[2] + ".";
+
+
+ }
+
+   console.log(s.length + "test print");
     return{
       text: str + s
-    }
+   }
   }
 }
 

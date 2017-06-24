c292152cc59478d0bef0fb61e61ff1c405c2b1e8
RomeDorado <romedorado@gmail.com>
2017-06-24 23:55:41 +0800

    asdasd

--- fbeamer/index.js
+++ fbeamer/index.js
@@ -167,10 +167,37 @@ card(id, messagedata) {
 				messagedata
 			}
 		}
-			this.sendMessage(obj)
+			this.callSendAPI(obj)
 			.catch(error => console.log(error) + "This is the error");
 			
 	}
+
+	callSendAPI(messageData) {
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {
+			access_token: config.FB_PAGE_TOKEN
+		},
+		method: 'POST',
+		json: messageData
+
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var recipientId = body.recipient_id;
+			var messageId = body.message_id;
+
+			if (messageId) {
+				console.log("Successfully sent message with id %s to recipient %s",
+					messageId, recipientId);
+			} else {
+				console.log("Successfully called Send API for recipient %s",
+					recipientId);
+			}
+		} else {
+			console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+		}
+	});
+}
 			
 	
 

f83471d8486b213463d38ac2def33c7bc69e4179
RomeDorado <romedorado@gmail.com>
2017-06-24 23:41:34 +0800

    pull

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ server.post('/', (req, res, next) => {
 			wit.message(message.text, {})
 				.then(omdb)
 				.then(response => {					
-					//console.log(response);
+					console.log(response + "This is the response");
 					f.txt(sender, response.text);
 					if(response.image) {
 						f.img(sender, response.image);

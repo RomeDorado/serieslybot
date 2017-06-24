d5d31a7b0226dd42ca7ef632902b38369c30f2c9
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-24 15:15:04 +0800

    asdf

--- movieList/index.js
+++ movieList/index.js
@@ -15,7 +15,7 @@ const createMovieList = (movieList) => {
     var imageURL = "http://image.tmdb.org/t/p/w185" + poster_path;
 
     let str = `Try watching these movies: `
-    console.log('ITO AND MOVIELIST:' + results.title);
+    console.log('ITO AND MOVIELIST:' + movieList.title);
 
     return{
       text: str,

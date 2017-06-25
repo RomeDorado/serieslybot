f4d1a2b3577e1c3fc27a5712177abd4607fd01f3
RomeDorado <romedorado@gmail.com>
2017-06-25 14:58:33 +0800

    if

--- person/index.js
+++ person/index.js
@@ -4,7 +4,7 @@ const request = require('request');
 const createBiography = require('../bio');
 const createResponsePerson = (intent, person) => {
   return new Promise((resolve, reject) => {
-    if(person){
+    if(person.Response === 'True'){
       let {
         results: [{
         id
